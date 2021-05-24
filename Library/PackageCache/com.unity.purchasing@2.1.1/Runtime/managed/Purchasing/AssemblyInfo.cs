a062cb71b8176b1876237ca4c3edcccd131d212f
Pico <46362299+picoxr@users.noreply.github.com>
2021-05-24 14:33:55 +0800

    update to PicoSDK2.8.9

--- Library/PackageCache/com.unity.purchasing@2.1.1/Runtime/managed/Purchasing/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("UnityEditor.Purchasing")]
-[assembly: InternalsVisibleTo("UnityEngine.Purchasing.Stores")]
-[assembly: InternalsVisibleTo("UnityEngine.Purchasing.RuntimeTests")]
